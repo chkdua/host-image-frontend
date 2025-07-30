388e1e4bb65b4be62dfa515de22a9aa1b39bb929
chkdua <144681664+chkdua@users.noreply.github.com>
2025-07-30 14:34:46 +0700

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,5 +1,5 @@
 // URL API kita
-const API_URL = "https://script.google.com/macros/s/AKfycby_R2i2Wc83e7Nlsxj3zWwJUwd7kBlQpM0dKDue3QUgijRZahbXkR4dZBr4RFlQWps/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz4U29eOsKIPA9eiZ7IgBik8fBYzT1rSzNjMnah5aP17fdiudP0y9i9fuX3TxdJ9U0/exec";
 
 // Ambil elemen dari halaman
 const uploadForm = document.getElementById('uploadForm');
